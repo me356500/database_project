29969834e441658541c50d581a5acb238594c0ef
hanson0820 <55423951+hanson0820@users.noreply.github.com>
2022-05-03 20:58:33 +0800

    Add files via upload

---
+++ shopadd.php
@@ -0,0 +1,34 @@
+
+<?php 
+header("Content-Type: text/html; charset=utf8");
+include 'config.php';
+$mealname=$_POST["mealname"];
+$price=$_POST["price"];   
+$quantity=$_POST["quantity"];  
+if(($mealname && $price && $quantity) == 0) {
+    echo "
+    <script> 
+        alert('Please fill all the blank!!');
+        location.href=  'nav.php?id=$uname';
+    </script>
+    
+    ";
+    
+    exit;
+}
+$sql = "INSERT INTO `goods` VALUES (NULL ,NULL, ?,?, ?);";
+$stmt = mysqli_stmt_init($link); 
+mysqli_stmt_prepare($stmt, $sql); 
+mysqli_stmt_bind_param($stmt, 'sii', $mealname, $price , $quantity ); 
+mysqli_stmt_execute($stmt); 
+$result = $stmt->get_result();
+mysqli_stmt_close($stmt);
+
+echo "
+<script> 
+    alert('ADD success !!');
+    location.href='index.html'
+</script>
+";
+?>
+
