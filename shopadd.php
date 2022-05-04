372b972ee7479e1783f0da0cac75414c9dc79a54
hanson0820 <55423951+hanson0820@users.noreply.github.com>
2022-05-04 11:10:57 +0800

    Update shopadd.php

--- shopadd.php
+++ shopadd.php
@@ -5,6 +5,12 @@ include 'config.php';
 $mealname=$_POST["mealname"];
 $price=$_POST["price"];   
 $quantity=$_POST["quantity"];  
+$file = fopen($_FILES["myFile"]["tmp_name"], "rb");
+$fileContents = fread($file, filesize($_FILES["myFile"]["tmp_name"])); 
+  //關閉圖片檔
+fclose($file);
+  //讀取出來的圖片資料必須使用base64_encode()函數加以編碼：圖片檔案資料編碼
+$fileContents = base64_encode($fileContents);
 if(($mealname && $price && $quantity) == 0) {
     echo "
     <script> 
@@ -16,10 +22,17 @@ if(($mealname && $price && $quantity) == 0) {
     
     exit;
 }
-$sql = "INSERT INTO `goods` VALUES (NULL ,NULL, ?,?, ?);";
+$imgType=$_FILES["upfile"]["type"];
+//$sql="INSERT INTO goods (img,imgType) VALUES ('$fileContents','$imgType')";
+
+//
+
+
+
+$sql = "INSERT INTO `goods` VALUES (NULL ,NULL, ?,?, ?,?,?);";
 $stmt = mysqli_stmt_init($link); 
 mysqli_stmt_prepare($stmt, $sql); 
-mysqli_stmt_bind_param($stmt, 'sii', $mealname, $price , $quantity ); 
+mysqli_stmt_bind_param($stmt, 'siiss', $mealname, $price , $quantity,$fileContents, $imgType ); 
 mysqli_stmt_execute($stmt); 
 $result = $stmt->get_result();
 mysqli_stmt_close($stmt);
@@ -29,6 +42,17 @@ echo "
     alert('ADD success !!');
     location.href='index.html'
 </script>
-";
+";/*
+if($conn->query($sql) === TRUE) {
+    echo "成功";
+} 
+else {
+  //echo "Error: " . $sql . "<br>" . $conn->error;
+    echo "失敗";
+}*/
+$conn->close();
+
+
+
 ?>
 
