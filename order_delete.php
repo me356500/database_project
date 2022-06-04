02482a450f7fd48c9292537c48b5b2f3d1669224
hanson0820 <55423951+hanson0820@users.noreply.github.com>
2022-06-04 14:22:26 +0800

    Update order_delete.php

--- order_delete.php
+++ order_delete.php
@@ -3,9 +3,19 @@ header("Content-Type: text/html; charset=utf8");
 include 'config.php';
 $pid=$_POST["pid"];
 $account = $_POST["account"];
+$price = $_POST["price"];
 
-$sql = "update order_list SET state ='cancel' ";
+$sql = "update order_list SET state ='cancel' ,end_time=NULL ";
 $data1 = mysqli_query($link, $sql);
+$sql = "update user,order_list set user.balance = user.balance + ? where user.UID=order_list.UID";
+$stmt = mysqli_stmt_init($link); 
+mysqli_stmt_prepare($stmt, $sql); 
+mysqli_stmt_bind_param($stmt, 'i',$price ); 
+mysqli_stmt_execute($stmt); 
+$result = $stmt->get_result();
+mysqli_stmt_close($stmt);
+
+
 echo "
 <script> 
     alert('cancel success !!');
